6af114a83b27e165b1b43b50dc7385b502c4da01
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-10-19 13:42:12 -0400

    remove gethostbyname()

--- exo7.py
+++ exo7.py
@@ -4,14 +4,12 @@ from scapy.all import ICMP , IP
 import scapy.sendrecv
 
 hostname = argv[1]
-host = socket.gethostbyname(hostname)
 
-print(hostname, " -> ", host)
-packet = IP(dst=host, ttl = (1,20))/ICMP(type='echo-request')
+packet = IP(dst=hostname, ttl = (1,20))/ICMP(type='echo-request')
 
 for pkt in packet:
     result = scapy.sendrecv.sr1(pkt, verbose=False, timeout=2)
     if(result == None): print("TTL \t", pkt[0].ttl, "\t****\t ICMP")
     else: 
         print("TTL \t", pkt[0].ttl, "\t", result[0].src, "\t ICMP")
-        if(result[0].src == host): break
+        if(result[0].src == socket.gethostbyname(hostname)): break
