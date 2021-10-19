6aac3619d0ef6fc825d8ba82e9ea5a1bb7c980b8
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-10-19 13:43:46 -0400

    change iface

--- exo6.py
+++ exo6.py
@@ -3,12 +3,12 @@ from struct import pack
 from sys import argv
 from scapy.all import ARP, Ether, IP, srp
 
-interface = iface=argv[2]
-ip = iface=argv[3]
+interface = iface=argv[1]
+ip = iface=argv[2]
 
 packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip)
 
-result = srp(packet, iface="en0", timeout=3, verbose=0)[0]
+result = srp(packet, iface=interface, timeout=3, verbose=0)[0]
 
 if result:
     for sent, received in result:
