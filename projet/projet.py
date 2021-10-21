7e77607c64c48133d9415083b447b6f5471731fa
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2021-10-21 17:34:01 -0400

    add file creation

--- projet/projet.py
+++ projet/projet.py
@@ -1,25 +1,34 @@
 import argparse
+import os
 from scapy.all import *
 
 pcap = rdpcap("tftp.pcap")
 i = 0
 dst_port = 0
 dst_addr = 0
+buffer = ""
 
-print(pcap[9].show())
-print(pcap[9][Raw].load)
-
+#print(pcap[9].show())
+#print(pcap[9][Raw].load.decode("utf-8"))
 
 for pck in pcap:
     if(pck[Ether].type == 2048):
         if(pck[IP].proto == 17): 
             if(pck[UDP].dport == 69): 
-                if(i > 0): print(f"{filename} : {i} packets ({dst_addr}:{dst_port})")
+                if(i > 0): 
+                    print(f"{filename} : {i} packets ({dst_addr}:{dst_port})")
+                    if('/' in filename):
+                        if not os.path.exists(os.path.dirname(filename)):
+                            os.makedirs(os.path.dirname(filename))
+                    with open(filename, 'w') as file:
+                        file.write(buffer)
+                        buffer = ""
                 dst_port = pck[UDP].sport
                 dst_addr = pck[IP].src
-                filename = pck[TFTP].filename
+                filename = pck[TFTP].filename.decode("utf-8")
                 i = 0
             elif("File Not Found" in pck.sprintf("%load%")):
                 print(f"{filename} : File Not Found")
             elif(pck[UDP].dport == dst_port and pck[IP].dst == dst_addr and "blksize" not in pck.sprintf("%load%")): 
+                buffer += pck[Raw].load.decode('utf-8', 'replace')
                 i += 1
